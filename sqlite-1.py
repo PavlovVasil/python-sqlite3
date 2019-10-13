8573ea2af0eba8d3cc04af20636bb33251e60b24
PavlovVasil <vasil@vasilpavlov.com>
2019-10-13 23:19:53 +0300

    Add dynamic data generation

--- sqlite-1.py
+++ sqlite-1.py
@@ -1,4 +1,7 @@
 import sqlite3
+import time
+import datetime
+import random
 
 conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
@@ -9,8 +12,24 @@ def create_table():
 def data_entry():
     c.execute("INSERT INTO stuffToPlot VALUES(145123542, '2019-10-13', 'Python', 5)")
     conn.commit()
-    c.close()
-    conn.close()
+    # c.close()
+    # conn.close()
+
+def dynamic_data_entry():
+    unix = time.time()
+    date = str(datetime.datetime.fromtimestamp(unix).strftime("%Y-%m-%d %H:%M:%S"))
+    keyword = "Python"
+    value = random.randrange(0, 10)
+    c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)", 
+        (unix, date, keyword, value))
+    conn.commit()
 
 create_table()
-data_entry()
+# data_entry()
+
+for i in range(10):
+    dynamic_data_entry()
+    time.sleep(1)
+
+c.close()
+conn.close()
